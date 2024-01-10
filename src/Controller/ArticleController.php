49d4ef2bdcc33bc93b156f8f0f6ffdb6ae268bc3
Niceleyy <david.wu@edu.univ-paris13.fr>
2024-01-10 16:00:03 +0100

    Création d'article

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -7,35 +7,61 @@ namespace App\Controller;
 use App\Entity\Article;
 use App\Form\ArticleType;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 #[Route("/article", name:"article_")]
 class ArticleController extends AbstractController
 {
+    private $client;
+
+    public function __construct(HttpClientInterface $client)
+    {
+        $this->client = $client;
+    }
     
     #[Route("/new", name:"new")]
     #[IsGranted("ROLE_USER")]
     public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
-        $article = new Article();
-        $form = $this->createForm(ArticleType::class, $article);
+
+        $form = $this->createForm(ArticleType::class);
 
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
+            // Data is already set in the $article object
+            // Retrieve form data
+            $title = $form->get('title')->getData();
+            $description = $form->get('description')->getData();
+            $body = $form->get('body')->getData();
 
-            dd($form->get('title')->getData());
+            try {
+                $response = $this->client->request('POST', 'api/articles', [
+                    'headers' => [
+                        'Content-Type' => 'application/json',
+                    ],
+                    'json' => [ 
+                    
+                    'title' => $title,
+                    'description' => $description,
+                    'body' => $body]
+                ]);
 
-            $entityManager->persist($article);
-            $entityManager->flush();
-
-            // Redirect after saving the article
-            return $this->redirectToRoute('app_index');
+                if ($response->getStatusCode() === 200) {
+                    return $this->redirectToRoute('app_index');
+                } else {
+                    dd('fuck u1');
+                }
+            } catch (\Throwable $e) {
+                dd('fuck u2');
+                // Handle exceptions
+                // Log the error or display a message
+            }
         }
-
         return $this->render('article/new.html.twig', [
             'form' => $form->createView(),
         ]);
--- src/Form/ArticleType.php
+++ src/Form/ArticleType.php
@@ -5,7 +5,6 @@ namespace App\Form;
 use App\Entity\Article;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
@@ -15,18 +14,16 @@ class ArticleType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title', TextType::class, [
+            ->add(
+                'title', TextType::class, [
+                    'mapped' => false
+                ])
+            ->add('description', TextType::class, [
                 'mapped' => false
             ])
-            ->add('description', TextType::class)
-            ->add('body', TextareaType::class)
+            ->add('body', TextareaType::class, [
+                'mapped' => false
+            ])
             ->add('save', SubmitType::class, ['label' => 'Create Article']);
-    }
-
-    public function configureOptions(OptionsResolver $resolver): void
-    {
-        $resolver->setDefaults([
-            'data_class' => Article::class,
-        ]);
     }
 }
